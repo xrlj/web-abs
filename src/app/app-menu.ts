81e5aee43d9f323662169a816d271a9b1f0418c1
xinxiamu <932852117@qq.com>
2019-11-15 01:12:08 +0800

    提交

--- src/app/app-menu.ts
+++ src/app/app-menu.ts
@@ -6,7 +6,7 @@ export const APP_MENUS = [
     link: '/'
   },
   {
-    title: '会员管理',
+    title: '用户管理',
     icon: 'appstore',
     children: [
       { title: '企业管理', link: '/navigation/affix' },
@@ -14,32 +14,31 @@ export const APP_MENUS = [
     ]
   },
   {
-    title: '产品管理',
+    title: '贷前管理',
     icon: 'appstore',
     children: [
-      { title: '产品列表', link: '/navigation/affix' }
+      { title: '授信协议管理', link: '/navigation/affix' },
+      { title: '转让合同管理', link: '/navigation/breadcrumb' },
+      { title: '产品管理', link: '/navigation/affix' }
     ]
   },
   {
-    title: '融资管理',
+    title: '贷中管理',
     icon: 'box-plot',
     children: [
       { title: '可融资数据', link: '/navigation/affix' },
-      { title: '受让应收账款', link: '/navigation/affix' }
+      { title: '转让管理', link: '/navigation/affix' }
     ]
   },
   {
-    title: '资产管理',
+    title: '贷后管理',
     icon: 'box-plot',
     children: [
-      { title: '资产列表', link: '/navigation/affix' }
-    ]
-  },
-  {
-    title: '基础管理',
-    icon: 'box-plot',
-    children: [
-      { title: '产品类型管理', link: '/navigation/affix' }
+      { title: '资产管理', link: '/navigation/affix' },
+      { title: '中登网登记', link: '/navigation/affix' },
+      { title: '银行流水登记', link: '/navigation/affix' },
+      { title: '汇款核销', link: '/navigation/affix' },
+      { title: '客户台账', link: '/navigation/affix' }
     ]
   },
   {
@@ -50,8 +49,7 @@ export const APP_MENUS = [
       { title: '部门管理', link: '/navigation/dropdown' },
       { title: '角色管理', link: '/navigation/breadcrumb' },
       { title: '权限管理', link: '/navigation/dropdown' },
-      { title: '菜单管理', link: '/navigation/menu' },
-      { title: '应用管理', link: '/navigation/menu' }
+      { title: '菜单管理', link: '/navigation/menu' }
     ]
   }
 ];
--- src/app/theme/components/app-header/app-header.component.ts
+++ src/app/theme/components/app-header/app-header.component.ts
@@ -8,5 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppHeaderComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  userTypeName: string;
+
+  ngOnInit() {
+    this.userTypeName = '保理商';
+  }
 }
