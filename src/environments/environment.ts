5ca6edeb50c7dd19e69f56cc35b9090b464e8b14
xinxiamu <932852117@qq.com>
2020-07-26 19:46:33 +0800

    好好学习，天天向上

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,6 @@ export const environment = {
   // apiUrl: 'http://api-dev.xrlj.com:5555'
   apiUrl: 'http://localhost:5555'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
